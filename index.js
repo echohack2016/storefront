d023ffb74a9f6743f1b0b554e27696c6b6716cf8
Steven de Salas <steven@desalasworks.com>
2016-04-16 15:13:28 +1000

    Initial commit - basic html site

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var port = process.env.PORT || 5000;
+var app = express();
+
+
+app.set('port', port);
+
+app.use('/', express.static(__dirname + '/www'));
+
+app.listen(port, function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
